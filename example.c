14b0242da691d7dc48bafe5092de1428ec05631f
codeandkey <jtst@iastate.edu>
2021-12-23 16:55:43 -0600

    add more comments to example.c

--- example.c
+++ example.c
@@ -1,7 +1,10 @@
 #include "mtest.h"
 
-#include <cmath>
+#include <math.h>
 
+/**
+ * Primality tester - we will test this function.
+ */
 int is_prime(int n) {
   if (n < 2)
     return 0;
@@ -18,16 +21,26 @@ int is_prime(int n) {
   return 1;
 }
 
+// Use TEST(MyTestName) to define tests, just like functions.
+// Tests do not have a return value, however you can still 'return'
+// to abort a test.
+
 TEST(IsPrimeTest) {
+  // EXPECT() allows us to test a condition which _should_ be true;
+  // if it is not, we note the failure but continue on in the test.
+ 
   EXPECT(is_prime(3));
   EXPECT(is_prime(5));
   EXPECT(is_prime(7));
-  EXPECT(is_prime(4392));
+  EXPECT(is_prime(4392)); // this number is not prime
   EXPECT(is_prime(4002679));
   EXPECT(is_prime(40000003));
 }
 
 TEST(BasicTest) {
+  // ASSERT() allows us to test a condition, but if the assertion
+  // fails the test is immediately aborted.
+
   EXPECT(100);
   EXPECT(200);
   EXPECT(0);
@@ -35,10 +48,12 @@ TEST(BasicTest) {
   EXPECT(20); // unreachable
 }
 
+// Test which always passes
 TEST(OkTest) {
   EXPECT(1);
 }
 
+// Some tests which take longer
 TEST(LongTest1) {
     for (int j = 0; j < 100000000; ++j) { j += 1; j -= 1; }
 }
@@ -55,4 +70,6 @@ TEST(LongTest4) {
     for (int j = 0; j < 100000000; ++j) { j += 1; j -= 1; }
 }
 
+// To run your tests, call mtest_main() with the command-line arguments.
+// The value returned from mtest_main() should be used as the exit code.
 int main(int argc, char **argv) { return mtest_main(argc, argv); }
